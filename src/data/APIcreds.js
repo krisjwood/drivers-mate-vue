6a5dd9a36be884a4d4dc32adb34e95bad87e342b
krisjwood <kriswd0@gmail.com>
2022-08-07 23:35:44 +1200

    api creds file

---
+++ src/data/APIcreds.js
@@ -0,0 +1,7 @@
+export default {
+    rootUrl: "https://6kjiq6rvyh.execute-api.ap-southeast-2.amazonaws.com/prod",
+    headers: {
+      Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJOZXRMb2dpeCIsImlhdCI6MTY0Nzc0MTI2MiwiZXhwIjoxODA1NTA3NjYyLCJhdWQiOiJOZXRMb2dpeCIsInN1YiI6Ik5ldGxvZ2l4QXNzZXNzbWVudHMifQ.cv9FkxMIJk0BA6u-C0UDC3Ao9YixLXjGpkXW2hk1fYs",
+      accept: "application/json",
+    }
+  }
