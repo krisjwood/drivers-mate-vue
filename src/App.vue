6173cdc3b71a38eab952eeb2a311babfa6de3014
krisjwood <kriswd0@gmail.com>
2022-08-09 01:03:03 +1200

    desktop warning

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,70 @@
 <template>
   <div>
-    <router-view/>
-    <div class="footer-wrapper">
-      <span>Kris Wood &copy; 2022</span>
+    <div class="overlay" v-show="show">
+      <b-overlay show rounded="sm">
+        <router-view/>
+        <div class="footer-wrapper">
+          <span>Kris Wood &copy; 2022</span>
+        </div>
+      <template #overlay>
+        <div class="text-center">
+          <div class="row justify-content-center">
+              <i class="fa-solid fa-triangle-exclamation fa-fade"></i>
+            <h2>This application is intended for mobile and tablet use only.</h2>
+            <h3>Viewing on a desktop may be difficult to navigate or result in unexpected behaviour.</h3>
+          </div>
+          <div>
+            <b-button
+              class="mt-5"
+              ref="cancel"
+              size="sm"
+              aria-describedby="cancel-label"
+              @click="closeOverlay()"
+              squared
+              >
+              Cancel
+            </b-button>
+          </div>
+        </div>
+      </template>
+    </b-overlay>
+    </div>
+    <div else>
+      <router-view/>
+      <div class="footer-wrapper">
+        <span>Kris Wood &copy; 2022</span>
+      </div>
     </div>
   </div>
 </template>
+<script>
+
+export default {
+  data() {
+    return {
+      show: false,
+    }
+  },
+  created() {
+    this.checkIfDesktop()
+  },
+  methods: {
+    checkIfDesktop() {
+      if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        this.show = true;
+      }
+    },
+    closeOverlay() {
+      this.show = false;
+    }
+  }
+}
+</script>
 <style>
+  .fa-triangle-exclamation {
+    font-size: 10rem;
+    color: rgba(244, 112, 40);
+  }
   .footer-wrapper {
     position: static;
     left: 0;
