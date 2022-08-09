e0cb91e2e3925de3924a22a7e151b703d133d05b
krisjwood <kriswd0@gmail.com>
2022-08-10 00:06:06 +1200

    Style user management

--- src/components/ChildPageHeader.vue
+++ src/components/ChildPageHeader.vue
@@ -4,14 +4,14 @@
       <div class="col-8">
         <img class="h-auto w-100" src="../assets/drivers-mate-nl-logo-child.png" alt="Netlogix Driver's Mate Logo">
       </div>
-      <div class="col">
-        <div class="row d-flex justify-content-end align-items-center mr-3">
-          <b-avatar size="1rem"></b-avatar>
-          <div class="ml-1"><b>{{ display.selectedDriver }}</b></div>
-        </div>
+      <div class="col d-flex justify-content-end mr-1 mb-1 py-0">
+          <b-button squared size="sm" class="p-0 user-button">
+            <b-avatar class="user-icon" size="md" square button @click="userManagement"></b-avatar>
+            <b-badge class="mr-1" variant="warning" pill>6</b-badge>
+          </b-button>
       </div>
-    </div>
-    <div class="d-flex justify-content-center align-items-center mb-2 title-wrapper">
+      </div>
+    <div v-if="show" class="d-flex justify-content-center align-items-center mb-2 title-wrapper">
       <h1>{{ title }}</h1>
     </div>
   </div>
@@ -22,6 +22,10 @@
     props: {
       title: {
         type: String,
+        required: true,
+      },
+      show: {
+        type: Boolean,
         required: true,
       }
     },
@@ -36,6 +40,17 @@
       if (sessionStorage.getItem('selectedDriver')) {
         this.display.selectedDriver = sessionStorage.getItem("selectedDriver");
       }
+    },
+    methods: {
+      userManagement() {
+        this.$bvModal.msgBoxOk("Sorry, user management hasn't been released yet. Check back soon!", {
+          title: '6 notifications',
+          size: 'md',
+          buttonSize: 'sm',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0'
+        })
+      }
     }
   }
 </script>
@@ -47,4 +62,19 @@
   .title-wrapper {
     background-color: rgba(19, 28, 69);
   }
+
+  .user-button:hover {
+    background-color: rgba(244, 112, 40) !important;
+  }
+
+  .user-icon:hover {
+    background-color: rgba(244, 112, 40) !important;
+    color: rgba(19, 28, 69) !important;
+  }
+
+  .user-button:active {
+    border: none;
+  }
+
+
 </style>
