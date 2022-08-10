358d3e8d36ef9e49b6373ee4433a32f19d96614d
krisjwood <kriswd0@gmail.com>
2022-08-10 19:44:20 +1200

    consignment tests

---
+++ tests/unit/ConsignmentItem.spec.js
@@ -0,0 +1,49 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import ConsignmentItem from '@/components/ConsignmentItem'
+import BootstrapVue from 'bootstrap-vue'
+
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+
+describe('ConsignmentItem.vue', () => {
+  const consignment =
+    {
+      Items: [
+        {
+          Quantity: 20,
+          ItemCode: "AMZ-01"
+        },
+      ],
+      TripId: "123456",
+      DeliveryAddress: {
+        Street: "Over the hill street",
+        City: "Shelbyville",
+        Unit: "66",
+        Suburb: "Mountaintop Place",
+        Postcode: "2013"
+      },
+      RequestedPickupTime: "2022/02/03 07:00:00",
+      PickupInstructions: "Make sure driver signs in before heading to loading bay",
+      Status: "TripAllocated",
+      PickupAddress: {
+        Street: "Happy Vallssssey Road",
+        City: "Springfield",
+        Unit: "14",
+        Suburb: "Sunshine Place",
+        Postcode: "1023"
+      },
+      DeliveryInstructions: "Items are fragile, take extra care when unloading",
+      OrderId: "CH-1010"
+    };
+  it('is a Vue instance', () => {
+    const wrapper = mount(ConsignmentItem, {
+      localVue,
+      propsData: {
+        consignment,
+        index: 1,
+        quantity: 3,
+      },
+    })
+    expect(wrapper.find('ConsignmentItem').vm)
+  })
+})
--- tests/unit/TripConsignment.spec.js
+++ tests/unit/TripConsignment.spec.js
@@ -1,14 +1,20 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import TripConsignment from '@/views/TripConsignment'
 import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+
 
 const localVue = createLocalVue()
+const router = new VueRouter()
+
 localVue.use(BootstrapVue)
+localVue.use(VueRouter)
 
 describe('TripConsignment.vue', () => {
   it('is a Vue instance', () => {
     const wrapper = mount(TripConsignment, {
       localVue,
+      router,
       mocks: {
         $route: {
           params: { OrderId: '123' }
